d456fd00628c9ae69603add49de8d3e737d53503
DeeJayJay <deejayjay@zoho.com>
2022-08-21 16:19:27 -0600

    Adds sidenav, search bar, hamburger menu for mobile layout.

--- src/js/dashboard.js
+++ src/js/dashboard.js
@@ -1,22 +1,42 @@
-console.log("Welcome to Admin Dashboard page");
-const { createApp } = Vue;
+const btnMenu = document.getElementById("btnMenu");
+const btnClose = document.getElementById("btnClose");
+const btnMobileLogout = document.getElementById("btnMobileLogout");
+const overlay = document.getElementById("sideNavOverlay");
+const sideNav = document.getElementById("sideNav");
 
-const app = createApp({
-  data() {
-    return {
-      names: ["Walter White", "Saul Goodman", "Jesse Pinkman", "Hank Schrader", "Walter White Jr.", "Mike Ehrmantraut"],
-      selectedNameIndex: null
-    }
-  },
-  methods: {
-    handleNameClick(index) {
-      this.selectedNameIndex = index;
-    },
-    deleteUser() {
-      this.names.splice(this.selectedNameIndex, 1);
-      this.selectedNameIndex = null;
-    }
-  }
+btnMenu.addEventListener("click", () => {
+  overlay.classList.add("mobile-sidenav__active");
+  sideNav.style.width = "80%";
 });
 
-app.mount('#app');
+btnClose.addEventListener("click", () => {
+  overlay.classList.remove("mobile-sidenav__active");
+  sideNav.style.width = "0";
+});
+
+btnMobileLogout.addEventListener("click", () => {
+  console.log("click() - btnMobileLogout");
+  // Add logic to logout user
+});
+
+// const { createApp } = Vue;
+
+// const app = createApp({
+//   data() {
+//     return {
+//       names: ["Walter White", "Saul Goodman", "Jesse Pinkman", "Hank Schrader", "Walter White Jr.", "Mike Ehrmantraut"],
+//       selectedNameIndex: null
+//     }
+//   },
+//   methods: {
+//     handleNameClick(index) {
+//       this.selectedNameIndex = index;
+//     },
+//     deleteUser() {
+//       this.names.splice(this.selectedNameIndex, 1);
+//       this.selectedNameIndex = null;
+//     }
+//   }
+// });
+
+// app.mount('#app');
