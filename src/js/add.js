46aeee37ce259d024cf43ddf93c6c6ca26b3b005
YitongXX <100065081+YitongXX@users.noreply.github.com>
2022-08-29 13:00:45 +0800

    initial commit - image upload

--- src/js/add.js
+++ src/js/add.js
@@ -1,1 +1,13 @@
 console.log("Welcome to Admin Add page");
+
+const image_input = document.querySelector("#image-input");
+
+image_input.addEventListener("change", function() {
+  const reader = new FileReader();
+  reader.addEventListener("load", () => {
+    const uploaded_image = reader.result;
+    document.querySelector(".display-image").style.backgroundImage = `url(${uploaded_image})`;
+  });
+  reader.readAsDataURL(this.files[0]);
+});
+
