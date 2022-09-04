500dfe6604e26917f4750c03095646417a635ff6
DeeJayJay <deejayjay@zoho.com>
2022-09-04 13:04:41 -0600

    Adds the code to save new album data
    Adds the code to add new album to firebase. Minor fixes to the style & HTML.

--- src/js/add.js
+++ src/js/add.js
@@ -5,9 +5,11 @@ import {
   getDownloadURL
 } from "firebase/storage";
 import { db, storage } from "./libs/firebaseConfig";
+import Toastify from "toastify-js";
+import "toastify-js/src/toastify.css";
 
+// Show image preview when an image is selected
 const imageInput = document.querySelector("#imageInput");
-
 imageInput.addEventListener("change", function () {
   const reader = new FileReader();
   reader.addEventListener("load", () => {
@@ -19,20 +21,105 @@ imageInput.addEventListener("change", function () {
   reader.readAsDataURL(this.files[0]);
 });
 
-// Save Album Handler
+
 const addAlbumForm = document.forms["addAlbumForm"];
 addAlbumForm.addEventListener("submit", handleSave);
 
+// Save Album Handler
 function handleSave(e) {
   e.preventDefault();
-  const albumName = addAlbumForm["albumName"].value.trim();
-  const artistName = addAlbumForm["artistName"].value.trim();
-  const genre = addAlbumForm["genre"].value.trim();
-  const release = addAlbumForm["release"].value;
-  const cost = addAlbumForm["cost"].value;
-  const desc = addAlbumForm["desc"].value.trim();
-  const imageFile = imageInput;
-  console.log({
-    albumName, artistName, genre, release, cost, desc, imageInput
-  });
+
+  let albumImage = null;
+  if (imageInput.files.length > 0) {
+    albumImage = imageInput.files[0];
+  }
+
+  const newAlbum = {
+    title: addAlbumForm["albumName"].value.trim(),
+    artist: addAlbumForm["artistName"].value.trim(),
+    genre: addAlbumForm["genre"].value.trim(),
+    release: Number(addAlbumForm["release"].value),
+    cost: Number(addAlbumForm["cost"].value),
+    description: addAlbumForm["desc"].value.trim(),
+    imgFile: albumImage
+  };
+
+  if (!validateInputs(newAlbum)) {
+    showNotification("Please fill all album details", "error");
+    return;
+  }
+
+  savenewAlbum(newAlbum);
+}
+
+// Validate all the data input by the user
+function validateInputs(album) {
+  const { title, artist, genre, release, cost, description, imgFile } = album;
+
+  const isValid =
+    title !== "" &&
+    artist !== "" &&
+    genre !== "" &&
+    release !== "" &&
+    cost !== "" &&
+    description !== "" &&
+    !!imgFile;
+
+  return isValid;
+}
+
+// Save the new album details to firebase RTD
+async function savenewAlbum({
+  title,
+  artist,
+  genre,
+  release,
+  cost,
+  description,
+  imgFile
+}) {
+  const dataRef = dbRef(db, "albums/");
+  const newAlbumRef = await push(dataRef); // Generates a unique key for the new album record
+  const imageRef = storageRef(storage, `images/albums/${newAlbumRef.key}`);
+
+  const uploadResult = await uploadBytes(imageRef, imgFile);
+  const imgStoragePath = uploadResult.metadata.fullPath;
+  const imgUrl = await getDownloadURL(imageRef);
+
+  // Save album details to RTD
+  set(newAlbumRef, {
+    id: newAlbumRef.key,
+    title,
+    artist,
+    genre,
+    release,
+    cost,
+    description,
+    imgUrl,
+    imgStoragePath
+  })
+    .then(() => {
+      showNotification("Album added successfully");
+    })
+    .catch((err) => {
+      console.log(err.message);
+      showNotification("Album could not be added", "error");
+    });
+}
+
+// Display success/error notifications
+function showNotification(message, type = "success") {
+  Toastify({
+    text: message,
+    duration: 3000,
+    newWindow: true,
+    close: true,
+    gravity: "top", // `top` or `bottom`
+    position: "center", // `left`, `center` or `right`
+    stopOnFocus: true, // Prevents dismissing of toast on hover
+    style: {
+      background: `${type === "error" ? "crimson" : "#00b09b"}`
+    },
+    onClick: function () { } // Callback after click
+  }).showToast();
 }
