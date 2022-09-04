4b54ee2e24f7d217406e43105a1a3bc50265a3c7
DeeJayJay <deejayjay@zoho.com>
2022-09-04 10:12:59 -0600

    Adds form event handler. Minor changes to the style.

--- src/js/add.js
+++ src/js/add.js
@@ -1,13 +1,38 @@
-console.log("Welcome to Admin Add page");
+import { ref as dbRef, push, set } from "firebase/database";
+import {
+  ref as storageRef,
+  uploadBytes,
+  getDownloadURL
+} from "firebase/storage";
+import { db, storage } from "./libs/firebaseConfig";
 
-const image_input = document.querySelector("#image-input");
+const imageInput = document.querySelector("#imageInput");
 
-image_input.addEventListener("change", function() {
+imageInput.addEventListener("change", function () {
   const reader = new FileReader();
   reader.addEventListener("load", () => {
-    const uploaded_image = reader.result;
-    document.querySelector(".display-image").style.backgroundImage = `url(${uploaded_image})`;
+    const uploadedImage = reader.result;
+    document.querySelector(
+      ".display-image"
+    ).style.backgroundImage = `url(${uploadedImage})`;
   });
   reader.readAsDataURL(this.files[0]);
 });
 
+// Save Album Handler
+const addAlbumForm = document.forms["addAlbumForm"];
+addAlbumForm.addEventListener("submit", handleSave);
+
+function handleSave(e) {
+  e.preventDefault();
+  const albumName = addAlbumForm["albumName"].value.trim();
+  const artistName = addAlbumForm["artistName"].value.trim();
+  const genre = addAlbumForm["genre"].value.trim();
+  const release = addAlbumForm["release"].value;
+  const cost = addAlbumForm["cost"].value;
+  const desc = addAlbumForm["desc"].value.trim();
+  const imageFile = imageInput;
+  console.log({
+    albumName, artistName, genre, release, cost, desc, imageInput
+  });
+}
