a08789d05cda9aafd201a7daaf0e5b77ae6ef785
DeeJayJay <deejayjay@zoho.com>
2022-09-04 09:11:30 -0600

    Adds demo of using Toastify.

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,18 @@
-console.log("Hello from JavaScript");
+import Toastify from 'toastify-js';
+import "toastify-js/src/toastify.css"
+
+const notification = Toastify({
+  text: "",
+  duration: 3000,
+  newWindow: true,
+  close: true,
+  gravity: "top", // `top` or `bottom`
+  position: "center", // `left`, `center` or `right`
+  stopOnFocus: true, // Prevents dismissing of toast on hover
+  style: {
+    background: "#00b09b",
+  },
+  onClick: function () { } // Callback after click
+});
+notification.options.text = "Add notification text here";
+notification.showToast();
