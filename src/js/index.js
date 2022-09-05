c18014d72478ec5026c1c3335d1da23ba9ae62e0
DeeJayJay <deejayjay@zoho.com>
2022-09-04 20:57:48 -0600

    Updates the index.js to use the showNotification function from util module,

--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,3 @@
-import Toastify from 'toastify-js';
-import "toastify-js/src/toastify.css"
+import { showNotification } from "./libs/utils";
 
-const notification = Toastify({
-  text: "",
-  duration: 3000,
-  newWindow: true,
-  close: true,
-  gravity: "top", // `top` or `bottom`
-  position: "center", // `left`, `center` or `right`
-  stopOnFocus: true, // Prevents dismissing of toast on hover
-  style: {
-    background: "#00b09b",
-  },
-  onClick: function () { } // Callback after click
-});
-notification.options.text = "Add notification text here";
-notification.showToast();
+showNotification("Add notification text here");
