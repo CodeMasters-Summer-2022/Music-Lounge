fc17d8472a6b416792f5264aeff73cb9270f23cf
YitongXX <100065081+YitongXX@users.noreply.github.com>
2022-09-11 10:25:11 +0800

    Add login function

--- src/js/login.js
+++ src/js/login.js
@@ -6,3 +6,18 @@ import { showNotification } from "./libs/utils";
 console.log(auth);
 
 showNotification("Login Page");
+
+const loginForm = document.querySelector(".loginForm");
+loginForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const email = loginForm.email.value;
+    const password = loginForm.password.value;
+
+    signInWithEmailAndPassword(auth, email, password)
+        .then(() => {
+            console.log("user logged in");
+        })
+        .catch((err) => {
+            console.log(err.message);
+        });
+});
